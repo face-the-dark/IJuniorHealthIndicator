3f5fb1ab26874add728f7224a40eb0ae3cd5546d
Nikolai <ne360cosmos@gmail.com>
2025-10-23 07:25:20 +0300

    Добавлены проверки на не отрицательность в методах класса здоровья

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -4,39 +4,33 @@ using UnityEngine;
 public class Health : MonoBehaviour
 {
     [SerializeField] private int _maxValue = 10;
-    
-    private int _currentValue;
 
-    public int MinValue => 0;
-    public int MaxValue => _maxValue;
-    
-    public event Action DamageTaken;
+    private int _currentValue;
     public event Action<int, int> HealthChanged;
 
-    private void Awake() => 
+    private void Awake() =>
         _currentValue = _maxValue;
 
-    private void Start() => 
+    private void Start() =>
         HealthChanged?.Invoke(_currentValue, _maxValue);
 
     public void TakeDamage(int damage)
     {
-        _currentValue -= damage;
+        if (damage < 0)
+            return;
 
-        if (_currentValue <= MinValue) 
-            _currentValue = MinValue;
-        
-        DamageTaken?.Invoke();
+        _currentValue = Mathf.Clamp(_currentValue - damage, 0, _maxValue);
+
         HealthChanged?.Invoke(_currentValue, _maxValue);
     }
 
     public void HealItself(int healValue)
     {
-        _currentValue += healValue;
-        
-        if (_currentValue > _maxValue)
-            _currentValue = _maxValue;
-        
+        if (healValue < 0)
+            return;
+
+        _currentValue = Mathf.Clamp(_currentValue + healValue, 0, _maxValue);
+
         HealthChanged?.Invoke(_currentValue, _maxValue);
     }
 }
